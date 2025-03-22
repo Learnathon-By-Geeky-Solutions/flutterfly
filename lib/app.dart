50fb0e5bfd66fc405354334bf79f5deb5d0e2d6a
raisarahman777 <raisarahman1904@gmail.com>
2025-03-22 22:25:45 +0600

    docs: add a new daily log

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'core/routes/route_generator.dart';
 import 'core/utils/theme/app_theme.dart';
 import 'l10n/generated/app_localizations.dart';
@@ -9,8 +10,7 @@ class App extends ConsumerWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref)
-  {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: router,
@@ -18,7 +18,7 @@ class App extends ConsumerWidget {
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
-      //locale: Locale('bn'),
+      // locale: Locale('bn'),
       localizationsDelegates: const [
         AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
